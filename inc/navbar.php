d8013023cba96917e0eda07b20c58dd1a0bb6488
Kazuki Przyborowski <kazuki.przyborowski@gmail.com>
2009-11-11 01:54:31 +0000

    Small bug fix to (/inc/navbar.php).
    git-svn-id: svn://svn.code.sf.net/p/intdb/svn/trunk@336 2b68903e-0b30-0410-9a39-a2e4f3c5be39

--- inc/navbar.php
+++ inc/navbar.php
@@ -11,14 +11,14 @@
     Copyright 2004-2009 iDB Support - http://idb.berlios.de/
     Copyright 2004-2009 Game Maker 2k - http://gamemaker2k.org/
 
-    $FileInfo: navbar.php - Last Update: 10/08/2009 SVN 324 - Author: cooldude2k $
+    $FileInfo: navbar.php - Last Update: 11/10/2009 SVN 336 - Author: cooldude2k $
 */
 $File3Name = basename($_SERVER['SCRIPT_NAME']);
 if ($File3Name=="navbar.php"||$File3Name=="/navbar.php") {
 	require('index.php');
 	exit(); }
 if($_SESSION['UserGroup']!=$Settings['GuestGroup']||$GroupInfo['CanPM']=="yes") {
-$pmquery1 = query("SELECT * FROM `".$Settings['sqltable']."messenger` WHERE `PMSentID`=%i AND `Read`=0", array($_SESSION['UserID']));
+$pmquery1 = query("SELECT * FROM `".$Settings['sqltable']."messenger` WHERE `ReciverID`=%i AND `Read`=0", array($_SESSION['UserID']));
 $pmresult1=mysql_query($pmquery1);
 $PMNumber=mysql_num_rows($pmresult1);
 @mysql_free_result($pmresult1); /*
