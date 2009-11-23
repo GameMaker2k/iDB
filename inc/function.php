300629da599ec774f4fd46445271156893ff6acf
Kazuki Przyborowski <kazuki.przyborowski@gmail.com>
2009-11-23 22:04:40 +0000

    Revorted some changes. :P
    git-svn-id: svn://svn.code.sf.net/p/intdb/svn/trunk@358 2b68903e-0b30-0410-9a39-a2e4f3c5be39

--- inc/function.php
+++ inc/function.php
@@ -11,7 +11,7 @@
     Copyright 2004-2009 iDB Support - http://idb.berlios.de/
     Copyright 2004-2009 Game Maker 2k - http://gamemaker2k.org/
 
-    $FileInfo: function.php - Last Update: 11/23/2009 SVN 357 - Author: cooldude2k $
+    $FileInfo: function.php - Last Update: 11/23/2009 SVN 358 - Author: cooldude2k $
 */
 $File3Name = basename($_SERVER['SCRIPT_NAME']);
 if ($File3Name=="function.php"||$File3Name=="/function.php") {
@@ -61,9 +61,9 @@ $REFERERurl = null;
 /**
   * Generates an UUID
   * 
-  * author     Anis uddin Ahmad <adminajaxray.com>
-  * param      string  an optional prefix
-  * return     string  the formatted uuid
+  * @author     Anis uddin Ahmad <admin@ajaxray.com>
+  * @param      string  an optional prefix
+  * @return     string  the formatted uuid
   */
   function uuid($useold = false,$more_entropy = false,$mtrand = false,$hash = 'sha1',$prefix = '') 
   {
