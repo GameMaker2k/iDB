e749c7797df6ac4d994395d8d8588dadf244e325
Kazuki Przyborowski <kazuki.przyborowski@gmail.com>
2009-12-05 17:34:03 +0000

    Another update to function chack_themes.
    git-svn-id: svn://svn.code.sf.net/p/intdb/svn/trunk@378 2b68903e-0b30-0410-9a39-a2e4f3c5be39

--- inc/function.php
+++ inc/function.php
@@ -11,7 +11,7 @@
     Copyright 2004-2009 iDB Support - http://idb.berlios.de/
     Copyright 2004-2009 Game Maker 2k - http://gamemaker2k.org/
 
-    $FileInfo: function.php - Last Update: 12/05/2009 SVN 377 - Author: cooldude2k $
+    $FileInfo: function.php - Last Update: 12/05/2009 SVN 378 - Author: cooldude2k $
 */
 $File3Name = basename($_SERVER['SCRIPT_NAME']);
 if ($File3Name=="function.php"||$File3Name=="/function.php") {
@@ -260,6 +260,8 @@ function chack_themes($theme) {
 global $BoardTheme;
 if(!isset($theme)) { $theme = null; }
 if(preg_match("/([a-zA-Z]+)\:/isU",$theme)) {
+	$theme = $BoardTheme; }
+if(!preg_match("/^[a-z0-9]+$/isU",$string)) {
 	$theme = $BoardTheme; }
 require('settings.php');
 $ckskindir = dirname(realpath("settings.php"))."/".$SettDir['themes'];
