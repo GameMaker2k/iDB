075f1b689a127e1d896ca4e0eca0a992b74d1613
Kazuki Przyborowski <kazuki.przyborowski@gmail.com>
2009-12-11 04:16:38 +0000

    Changed VACUUM to VACUUM ANALYZE.
    git-svn-id: svn://svn.code.sf.net/p/intdb/svn/trunk@394 2b68903e-0b30-0410-9a39-a2e4f3c5be39

--- inc/admin/main.php
+++ inc/admin/main.php
@@ -11,7 +11,7 @@
     Copyright 2004-2009 iDB Support - http://idb.berlios.de/
     Copyright 2004-2009 Game Maker 2k - http://gamemaker2k.org/
 
-    $FileInfo: main.php - Last Update: 12/10/2009 SVN 391 - Author: cooldude2k $
+    $FileInfo: main.php - Last Update: 12/10/2009 SVN 394 - Author: cooldude2k $
 */
 $File3Name = basename($_SERVER['SCRIPT_NAME']);
 if ($File3Name=="main.php"||$File3Name=="/main.php") {
@@ -107,7 +107,7 @@ if(isset($OptimizeAr[3])) { unset($OptimizeAr[3]); }
 if($Settings['sqltype']=="mysql") {
 $OptimizeTea = sql_query(sql_pre_query("OPTIMIZE TABLE \"".$TableChCk[$ti]."\"", array(null)),$SQLStat); }
 if($Settings['sqltype']=="pgsql") {
-$OptimizeTea = sql_query(sql_pre_query("VACUUM \"".$TableChCk[$ti]."\"", array(null)),$SQLStat); }
+$OptimizeTea = sql_query(sql_pre_query("VACUUM ANALYZE \"".$TableChCk[$ti]."\"", array(null)),$SQLStat); }
 $OptimizeAr = sql_fetch_array($OptimizeTea);
 if(!isset($OptimizeAr["Msg_text"])&&
 	isset($OptimizeAr[3])) { $OptimizeAr["Msg_text"] = $OptimizeAr[3]; }
