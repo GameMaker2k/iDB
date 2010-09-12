48535786972f3d31870d099ed5594eee9688e567
Kazuki Przyborowski <kazuki.przyborowski@gmail.com>
2010-09-12 00:51:13 +0000

    Version number fix. :P
    git-svn-id: svn://svn.code.sf.net/p/intdb/svn/trunk@540 2b68903e-0b30-0410-9a39-a2e4f3c5be39

--- inc/versioninfo.php
+++ inc/versioninfo.php
@@ -11,7 +11,7 @@
     Copyright 2004-2010 iDB Support - http://idb.berlios.de/
     Copyright 2004-2010 Game Maker 2k - http://gamemaker2k.org/
 
-    $FileInfo: versioninfo.php - Last Update: 09/10/2010 SVN 536 - Author: cooldude2k $
+    $FileInfo: versioninfo.php - Last Update: 09/10/2010 SVN 540 - Author: cooldude2k $
 */
 $File3Name = basename($_SERVER['SCRIPT_NAME']);
 if ($File3Name=="versioninfo.php"||$File3Name=="/versioninfo.php") {
