45b17f0fa3cf5b8ab9798cf0c9b06b22288053f4
Kazuki Przyborowski <kazuki.przyborowski@gmail.com>
2010-09-17 06:47:05 +0000

    Added new searchbots to list.
    git-svn-id: svn://svn.code.sf.net/p/intdb/svn/trunk@548 2b68903e-0b30-0410-9a39-a2e4f3c5be39

--- inc/misc/useragents.php
+++ inc/misc/useragents.php
@@ -11,10 +11,12 @@
     Copyright 2004-2010 iDB Support - http://idb.berlios.de/
     Copyright 2004-2010 Game Maker 2k - http://gamemaker2k.org/
 
-    $FileInfo: useragents.php - Last Update: 09/15/2010 SVN 545 - Author: cooldude2k $
+    $FileInfo: useragents.php - Last Update: 09/17/2010 SVN 548 - Author: cooldude2k $
 */
 // User Agent Checker
 function user_agent_check($user_agent) {
+if (preg_match("/".preg_quote("AboutUsBot")."/i", $user_agent)) {
+    return "AboutUs"; }
 if (preg_match("/".preg_quote("ia_archiver")."/i", $user_agent)) {
     return "Alexa"; }
 if (preg_match("/".preg_quote("AltaVista")."/i", $user_agent)) {
@@ -25,22 +27,38 @@ if (preg_match("/".preg_quote("Baiduspider")."/i", $user_agent)) {
     return "Baidu"; }
 if (preg_match("/".preg_quote("msnbot")."/i", $user_agent)) {
     return "Bing"; }
+if (preg_match("/".preg_quote("CCBot")."/i", $user_agent)) {
+    return "CommonCrawl"; }
 if (preg_match("/".preg_quote("CSE HTML Validator")."/i", $user_agent)) {
     return "CSE HTML Validator"; }
 if (preg_match("/".preg_quote("discobot")."/i", $user_agent)) {
     return "DiscoveryEngine"; }
 if (preg_match("/".preg_quote("Exabot")."/i", $user_agent)) {
     return "Exabot"; }
+if (preg_match("/".preg_quote("facebookexternalhit")."/i", $user_agent)) {
+    return "Facebook"; }
+if (preg_match("/".preg_quote("AdsBot-Google")."/i", $user_agent)) {
+    return "Google"; }
+if (preg_match("/".preg_quote("AppEngine-Google")."/i", $user_agent)) {
+    return "Google"; }
 if (preg_match("/".preg_quote("Googlebot")."/i", $user_agent)) {
     return "Google"; }
 if (preg_match("/".preg_quote("Mediapartners-Google")."/i", $user_agent)) {
     return "Google"; }
+if (preg_match("/".preg_quote("Hailoobot")."/i", $user_agent)) {
+    return "Hailoo"; }
 if (preg_match("|".preg_quote("HTML/XML Validator","|")."|i", $user_agent)) {
     return "HTML/XML Validator"; }
 if (preg_match("/".preg_quote("Lycos")."/i", $user_agent)) {
     return "Lycos"; }
 if (preg_match("/".preg_quote("montastic-webmonitor")."/i", $user_agent)) {
     return "Montastic"; }
+if (preg_match("/".preg_quote("smerity")."/i", $user_agent)) {
+    return "Schwa Lab"; }
+if (preg_match("/".preg_quote("ScoutJet")."/i", $user_agent)) {
+    return "ScoutJet"; }
+if (preg_match("/".preg_quote("Sogou")."/i", $user_agent)) {
+    return "Sogou"; }
 if (preg_match("/".preg_quote("Sosospider")."/i", $user_agent)) {
     return "Soso"; }
 if (preg_match("/".preg_quote("Speedy Spider")."/i", $user_agent)) {
@@ -49,14 +67,14 @@ if (preg_match("/".preg_quote("TotalValidator")."/i", $user_agent)) {
     return "Total Validator"; }
 if (preg_match("/".preg_quote("Validator.nu")."/i", $user_agent)) {
     return "Validator.nu"; }
+if (preg_match("/".preg_quote("W3C-checklink")."/i", $user_agent)) {
+    return "W3C Checklink"; }
 if (preg_match("/".preg_quote("FeedValidator")."/i", $user_agent)) {
     return "W3C Validator"; }
 if (preg_match("/".preg_quote("W3C_CSS_Validator")."/i", $user_agent)) {
     return "W3C Validator"; }
 if (preg_match("/".preg_quote("W3C_Validator")."/i", $user_agent)) {
     return "W3C Validator"; }
-if (preg_match("/".preg_quote("W3C-checklink")."/i", $user_agent)) {
-    return "W3C Checklink"; }
 if (preg_match("/".preg_quote("WDG_SiteValidator")."/i", $user_agent)) {
     return "WDG Validator"; }
 if (preg_match("/".preg_quote("WDG_Validator")."/i", $user_agent)) {
