e59cf6efeff630ead0b11b8bf09f6a99cc49d732
Kazuki Przyborowski <kazuki.przyborowski@gmail.com>
2010-10-04 22:05:23 +0000

    Yet another bug fix.
    git-svn-id: svn://svn.code.sf.net/p/intdb/svn/trunk@570 2b68903e-0b30-0410-9a39-a2e4f3c5be39

--- inc/versioninfo.php
+++ inc/versioninfo.php
@@ -11,7 +11,7 @@
     Copyright 2004-2010 iDB Support - http://idb.berlios.de/
     Copyright 2004-2010 Game Maker 2k - http://gamemaker2k.org/
 
-    $FileInfo: versioninfo.php - Last Update: 09/04/2010 SVN 569 - Author: cooldude2k $
+    $FileInfo: versioninfo.php - Last Update: 09/04/2010 SVN 570 - Author: cooldude2k $
 */
 $File3Name = basename($_SERVER['SCRIPT_NAME']);
 if ($File3Name=="versioninfo.php"||$File3Name=="/versioninfo.php") {
@@ -27,7 +27,7 @@ function version_info($proname,$subver,$ver,$supver,$reltype,$svnver,$showsvn) {
 	return $return_var; }
 // Version number and date stuff. :P
 $VER1[0] = 0; $VER1[1] = 4; $VER1[2] = 1; $VERFull[1] = $VER1[0].".".$VER1[1].".".$VER1[2];
-$VER2[0] = "Alpha"; $VER2[1] = "Al"; $VER2[2] = "SVN"; $SubVerN = 569; $RName = "iDB"; $SFName = "IntDB";
+$VER2[0] = "Alpha"; $VER2[1] = "Al"; $VER2[2] = "SVN"; $SubVerN = 570; $RName = "iDB"; $SFName = "IntDB";
 $SVNDay[0] = 09; $SVNDay[1] = 04; $SVNDay[2] = 2010; $SVNDay[3] = $SVNDay[0]."/".$SVNDay[1]."/".$SVNDay[2];
 $VerInfo['iDB_Ver'] = version_info($RName,$VER1[0],$VER1[1],$VER1[2],$VER2[1],$SubVerN,false);
 $VerInfo['iDB_Ver_SVN'] = version_info($RName,$VER1[0],$VER1[1],$VER1[2],$VER2[1],$SubVerN,true);
