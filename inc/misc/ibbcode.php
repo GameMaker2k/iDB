77827e1432324cbcb385ef231d544cc00cc53436
Kazuki Przyborowski <kazuki.przyborowski@gmail.com>
2010-11-29 21:31:56 +0000

    Small update.
    git-svn-id: svn://svn.code.sf.net/p/intdb/svn/trunk@599 2b68903e-0b30-0410-9a39-a2e4f3c5be39

--- inc/misc/ibbcode.php
+++ inc/misc/ibbcode.php
@@ -11,7 +11,7 @@
     Copyright 2004-2010 iDB Support - http://idb.berlios.de/
     Copyright 2004-2010 Game Maker 2k - http://gamemaker2k.org/
 
-    $FileInfo: ibbcode.php - Last Update: 11/05/2010 SVN 596 - Author: cooldude2k $
+    $FileInfo: ibbcode.php - Last Update: 11/06/2010 SVN 599 - Author: cooldude2k $
 */
 $File3Name = basename($_SERVER['SCRIPT_NAME']);
 if ($File3Name=="ibbcode.php"||$File3Name=="/ibbcode.php") {
@@ -23,9 +23,11 @@ This File has all the Functions for BBTags
 Thanks for the Help of czambran at: 
 http://www.phpfreaks.com/forums/index.php?action=profile;u=15535
 */ 
+$BoardCharSet = $Settings['charset'];
 function html_decode($matches)
 {
-return html_entity_decode($matches[1]);
+global $BoardCharSet;
+return html_entity_decode($matches[1], ENT_QUOTES, $BoardCharSet);
 }
 function do_html_bbcode($text)
 {
@@ -35,6 +37,14 @@ return $text;
 function bbcode_parser($text)
 {
 $text = preg_replace("/\[YouTube\]([A-Za-z0-9\.\-_]+)\[\/YouTube\]/is", "\n<object type=\"application/x-shockwave-flash\" width=\"480\" height=\"385\" data=\"http://www.youtube.com/v/\\1?fs=1&amp;hl=en_US\">\n<param name=\"\\1\" value=\"http://www.youtube.com/v/\\1?fs=1&amp;hl=en_US\" />\n</object>\n", $text);
+$text = preg_replace("/\[B\](.*?)\[\/B\]/is", "<span style=\"font-weight: bold;\">\\1</span>", $text);
+$text = preg_replace("/\[I\](.*?)\[\/I\]/is", "<span style=\"font-style: italic;\">\\1</span>", $text);
+$text = preg_replace("/\[S\](.*?)\[\/S\]/is", "<span style=\"font-style: strike;\">\\1</span>", $text);
+$text = preg_replace("/\[U\](.*?)\[\/U\]/is", "<span style=\"text-decoration: underline;\">\\1</span>", $text);
+$text = preg_replace("/\[O\](.*?)\[\/O\]/is", "<span style=\"text-decoration: overline;\">\\1</span>", $text);
+$text = preg_replace("/\[CENTER\](.*?)\[\/CENTER\]/is", "<span style=\"\">\\1</span>", $text);
+$text = preg_replace("/\[SIZE\=([0-9]+)\](.*?)\[\/SIZE\]/is", "<span style=\"\">\\1</span>", $text);
+$text = preg_replace("/\[COLOR\=([A-Za-z]+)\](.*?)\[\/COLOR\]/is", "<span style=\"\">\\1</span>", $text);
 return $text;
 }
 ?>
