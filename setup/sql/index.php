80b13892a56596637d921468b93663f844f3f11a
Kazuki Przyborowski <kazuki.przyborowski@gmail.com>
2011-06-08 07:49:26 +0000

    Small update.
    git-svn-id: svn://svn.code.sf.net/p/intdb/svn/trunk@661 2b68903e-0b30-0410-9a39-a2e4f3c5be39

---
+++ setup/sql/index.php
@@ -0,0 +1,18 @@
+<?php
+/*
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the Revised BSD License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    Revised BSD License for more details.
+
+    Copyright 2004-2011 iDB Support - http://idb.berlios.de/
+    Copyright 2004-2011 Game Maker 2k - http://gamemaker2k.org/
+    iDB Installer made by Game Maker 2k - http://idb.berlios.net/
+
+    $FileInfo: index.php - Last Update: 06/08/2011 SVN 661 - Author: cooldude2k $
+*/
+header('Location: ../../index.php');
+?>
