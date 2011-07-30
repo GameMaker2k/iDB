aa3cdfc36b29d48f26a454bbca05067aa7228e6c
Kazuki Przyborowski <kazuki.przyborowski@gmail.com>
2011-07-30 23:11:21 +0000

    Another small bug fix. :o
    git-svn-id: svn://svn.code.sf.net/p/intdb/svn/trunk@731 2b68903e-0b30-0410-9a39-a2e4f3c5be39

--- inc/iwrapper.php
+++ inc/iwrapper.php
@@ -11,7 +11,7 @@
     Copyright 2004-2011 iDB Support - http://idb.berlios.de/
     Copyright 2004-2011 Game Maker 2k - http://gamemaker2k.org/
 
-    $FileInfo: iwrapper.php - Last Update: 07/30/2011 SVN 729 - Author: cooldude2k $
+    $FileInfo: iwrapper.php - Last Update: 07/30/2011 SVN 731 - Author: cooldude2k $
 */
 $File3Name = basename($_SERVER['SCRIPT_NAME']);
 if ($File3Name=="iwrapper.php"||$File3Name=="/iwrapper.php") {
@@ -20,33 +20,35 @@ if ($File3Name=="iwrapper.php"||$File3Name=="/iwrapper.php") {
 
 if(!isset($ThemeSet['WrapperString'])||$ThemeSet['WrapperString']===null) {
 $ThemeSet['WrapperString'] = "<% HTMLSTART %>\n<% HTTPEQUIV %>\n<% METATAGS %>\n<% JAVASCRIPT %>\n<% LINKTAGS %>\n<% CSSTHEME %>\n<% FAVICON %>\n<% EXTRALINKS %>\n<% TITLETAG %>\n<% BODYTAG %>\n<% NAVBAR %>\n<% CONTENT %>\n<% COPYRIGHT %>\n<% HTMLEND %>"; }
+$iWrappers['WrapperString'] = $ThemeSet['WrapperString'];
 if(!isset($iWrappers['HTMLSTART'])) { $iWrappers['HTMLSTART'] = null; }
-$ThemeSet['WrapperString'] = preg_replace("/\<% HTMLSTART %\>/is", trim($iWrappers['HTMLSTART']), $ThemeSet['WrapperString']);
+$iWrappers['WrapperString'] = preg_replace("/\<% HTMLSTART %\>/is", trim($iWrappers['HTMLSTART']), $iWrappers['WrapperString']);
 if(!isset($iWrappers['HTMLSTART'])) { $iWrappers['HTTPEQUIV'] = null; }
-$ThemeSet['WrapperString'] = preg_replace("/\<% HTTPEQUIV %\>/is", trim($iWrappers['HTTPEQUIV']), $ThemeSet['WrapperString']);
+$iWrappers['WrapperString'] = preg_replace("/\<% HTTPEQUIV %\>/is", trim($iWrappers['HTTPEQUIV']), $iWrappers['WrapperString']);
 if(!isset($iWrappers['HTMLSTART'])) { $iWrappers['METATAGS'] = null; }
-$ThemeSet['WrapperString'] = preg_replace("/\<% METATAGS %\>/is", trim($iWrappers['METATAGS']), $ThemeSet['WrapperString']);
+$iWrappers['WrapperString'] = preg_replace("/\<% METATAGS %\>/is", trim($iWrappers['METATAGS']), $iWrappers['WrapperString']);
 if(!isset($iWrappers['HTMLSTART'])) { $iWrappers['JAVASCRIPT'] = null; }
-$ThemeSet['WrapperString'] = preg_replace("/\<% JAVASCRIPT %\>/is", trim($iWrappers['JAVASCRIPT']), $ThemeSet['WrapperString']);
+$iWrappers['WrapperString'] = preg_replace("/\<% JAVASCRIPT %\>/is", trim($iWrappers['JAVASCRIPT']), $iWrappers['WrapperString']);
 if(!isset($iWrappers['HTMLSTART'])) { $iWrappers['LINKTAGS'] = null; }
-$ThemeSet['WrapperString'] = preg_replace("/\<% LINKTAGS %\>/is", trim($iWrappers['LINKTAGS']), $ThemeSet['WrapperString']);
+$iWrappers['WrapperString'] = preg_replace("/\<% LINKTAGS %\>/is", trim($iWrappers['LINKTAGS']), $iWrappers['WrapperString']);
 if(!isset($iWrappers['HTMLSTART'])) { $iWrappers['CSSTHEME'] = null; }
-$ThemeSet['WrapperString'] = preg_replace("/\<% CSSTHEME %\>/is", trim($iWrappers['CSSTHEME']), $ThemeSet['WrapperString']);
+$iWrappers['WrapperString'] = preg_replace("/\<% CSSTHEME %\>/is", trim($iWrappers['CSSTHEME']), $iWrappers['WrapperString']);
 if(!isset($iWrappers['HTMLSTART'])) { $iWrappers['FAVICON'] = null; }
-$ThemeSet['WrapperString'] = preg_replace("/\<% FAVICON %\>/is", trim($iWrappers['FAVICON']), $ThemeSet['WrapperString']);
+$iWrappers['WrapperString'] = preg_replace("/\<% FAVICON %\>/is", trim($iWrappers['FAVICON']), $iWrappers['WrapperString']);
 if(!isset($iWrappers['EXTRALINKS'])) { $iWrappers['EXTRALINKS'] = null; }
-$ThemeSet['WrapperString'] = preg_replace("/\<% EXTRALINKS %\>/is", trim($iWrappers['EXTRALINKS']), $ThemeSet['WrapperString']);
+$iWrappers['WrapperString'] = preg_replace("/\<% EXTRALINKS %\>/is", trim($iWrappers['EXTRALINKS']), $iWrappers['WrapperString']);
 if(!isset($iWrappers['HTMLSTART'])) { $iWrappers['TITLETAG'] = null; }
-$ThemeSet['WrapperString'] = preg_replace("/\<% TITLETAG %\>/is", trim($iWrappers['TITLETAG']), $ThemeSet['WrapperString']);
+$iWrappers['WrapperString'] = preg_replace("/\<% TITLETAG %\>/is", trim($iWrappers['TITLETAG']), $iWrappers['WrapperString']);
 if(!isset($iWrappers['HTMLSTART'])) { $iWrappers['BODYTAG'] = null; }
-$ThemeSet['WrapperString'] = preg_replace("/\<% BODYTAG %\>/is", trim($iWrappers['BODYTAG']), $ThemeSet['WrapperString']);
+$iWrappers['WrapperString'] = preg_replace("/\<% BODYTAG %\>/is", trim($iWrappers['BODYTAG']), $iWrappers['WrapperString']);
 if(!isset($iWrappers['HTMLSTART'])) { $iWrappers['NAVBAR'] = null; }
-$ThemeSet['WrapperString'] = preg_replace("/\<% NAVBAR %\>/is", trim($iWrappers['NAVBAR']), $ThemeSet['WrapperString']);
+$iWrappers['WrapperString'] = preg_replace("/\<% NAVBAR %\>/is", trim($iWrappers['NAVBAR']), $iWrappers['WrapperString']);
 if(!isset($iWrappers['HTMLSTART'])) { $iWrappers['CONTENT'] = null; }
-$ThemeSet['WrapperString'] = preg_replace("/\<% CONTENT %\>/is", trim($iWrappers['CONTENT']), $ThemeSet['WrapperString']);
+$iWrappers['WrapperString'] = preg_replace("/\<% CONTENT %\>/is", trim($iWrappers['CONTENT']), $iWrappers['WrapperString']);
 if(!isset($iWrappers['HTMLSTART'])) { $iWrappers['COPYRIGHT'] = null; }
-$ThemeSet['WrapperString'] = preg_replace("/\<% COPYRIGHT %\>/is", trim($iWrappers['COPYRIGHT']), $ThemeSet['WrapperString']);
+$iWrappers['WrapperString'] = preg_replace("/\<% COPYRIGHT %\>/is", trim($iWrappers['COPYRIGHT']), $iWrappers['WrapperString']);
 if(!isset($iWrappers['HTMLSTART'])) { $iWrappers['HTMLEND'] = null; }
-$ThemeSet['WrapperString'] = preg_replace("/\<% HTMLEND %\>/is", trim($iWrappers['HTMLEND']), $ThemeSet['WrapperString']);
-echo $ThemeSet['WrapperString'];
+$iWrappers['WrapperString'] = preg_replace("/\<% HTMLEND %\>/is", trim($iWrappers['HTMLEND']), $iWrappers['WrapperString']);
+echo $iWrappers['WrapperString'];
+$iWrappers = array(null);
 ?>
