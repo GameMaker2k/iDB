77348332ba02fe67d116a30b578e05072d6bb5b6
Kazuki Przyborowski <kazuki.przyborowski@gmail.com>
2011-08-07 00:18:56 +0000

    Small update.
    git-svn-id: svn://svn.code.sf.net/p/intdb/svn/trunk@742 2b68903e-0b30-0410-9a39-a2e4f3c5be39

--- inc/admin/main.php
+++ inc/admin/main.php
@@ -11,7 +11,7 @@
     Copyright 2004-2011 iDB Support - http://idb.berlios.de/
     Copyright 2004-2011 Game Maker 2k - http://gamemaker2k.org/
 
-    $FileInfo: main.php - Last Update: 07/30/2011 SVN 729 - Author: cooldude2k $
+    $FileInfo: main.php - Last Update: 08/06/2011 SVN 742 - Author: cooldude2k $
 */
 $File3Name = basename($_SERVER['SCRIPT_NAME']);
 if ($File3Name=="main.php"||$File3Name=="/main.php") {
@@ -517,11 +517,11 @@ if(isset($_GET['menu'])&&$_GET['menu']=="main") { $AddChkURL = "&menu=main"; }
 <table style="text-align: left;">
 <tr>
 	<td style="width: 50%;"><span class="TextBoxLabel" title="Using User Time Zone">[User TimeZone] Install Date:</span></td>
-	<td style="width: 50%;"><?php echo GMTimeChange("F j Y, g:i a",$Settings['start_date'],$_SESSION['UserTimeZone'],0,$_SESSION['UserDST']); ?></td>
+	<td style="width: 50%;"><?php echo GMTimeChange("F j Y, ".$Settings['idb_time_format'],$Settings['start_date'],$_SESSION['UserTimeZone'],0,$_SESSION['UserDST']); ?></td>
 </tr><?php if($_SESSION['UserTimeZone']!=$Settings['DefaultTimeZone']||
 	$_SESSION['UserDST']!=$Settings['DefaultDST']) { ?><tr>
 	<td style="width: 50%;"><span class="TextBoxLabel" title="Using Board Time Zone">[Board TimeZone] Install Date:</span></td>
-	<td style="width: 50%;"><?php echo GMTimeChange("F j Y, g:i a",$Settings['start_date'],$Settings['DefaultTimeZone'],0,$Settings['DefaultDST']); ?></td>
+	<td style="width: 50%;"><?php echo GMTimeChange("F j Y, ".$Settings['idb_time_format'],$Settings['start_date'],$Settings['DefaultTimeZone'],0,$Settings['DefaultDST']); ?></td>
 </tr><?php } if($GroupInfo['ViewDBInfo']=="yes") { 
 ?><tr style="text-align: left;">
 	<td style="width: 50%;"><span class="TextBoxLabel">Forum Software Version:</span></td>
