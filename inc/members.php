92663f5adf403b67f510f5a17d236dd767b12558
Kazuki Przyborowski <kazuki.przyborowski@gmail.com>
2011-08-10 03:39:32 +0000

    Small bug fix.
    git-svn-id: svn://svn.code.sf.net/p/intdb/svn/trunk@746 2b68903e-0b30-0410-9a39-a2e4f3c5be39

--- inc/members.php
+++ inc/members.php
@@ -11,7 +11,7 @@
     Copyright 2004-2011 iDB Support - http://idb.berlios.de/
     Copyright 2004-2011 Game Maker 2k - http://gamemaker2k.org/
 
-    $FileInfo: members.php - Last Update: 08/08/2011 SVN 745 - Author: cooldude2k $
+    $FileInfo: members.php - Last Update: 08/09/2011 SVN 746 - Author: cooldude2k $
 */
 $File3Name = basename($_SERVER['SCRIPT_NAME']);
 if ($File3Name=="members.php"||$File3Name=="/members.php") {
@@ -610,8 +610,8 @@ $ViewMem['Joined']=GMTimeChange("M j Y, ".$Settings['idb_time_format'],$ViewMem[
 $ViewMem['LastActive']=sql_result($result,$i,"LastActive");
 $ViewMem['LastActive']=GMTimeChange("M j Y, ".$Settings['idb_time_format'],$ViewMem['LastActive'],$_SESSION['UserTimeZone'],0,$_SESSION['UserDST']);
 $ViewMem['Website']=sql_result($result,$i,"Website");
-if($MemList['Website']=="http://") { 
-	$MemList['Website'] = $Settings['idburl']; }
+if($ViewMem['Website']=="http://") { 
+	$ViewMem['Website'] = $Settings['idburl']; }
 $ViewMem['Website'] = urlcheck($ViewMem['Website']);
 $BoardWWWChCk = parse_url($Settings['idburl']);
 $MemsWWWChCk = parse_url($ViewMem['Website']);
--- inc/versioninfo.php
+++ inc/versioninfo.php
@@ -11,7 +11,7 @@
     Copyright 2008-2011 iDB Support - http://idb.berlios.de/
     Copyright 2008-2011 Game Maker 2k - http://gamemaker2k.org/
 
-    $FileInfo: versioninfo.php - Last Update: 08/08/2011 SVN 745 - Author: cooldude2k $
+    $FileInfo: versioninfo.php - Last Update: 08/09/2011 SVN 746 - Author: cooldude2k $
 */
 $File3Name = basename($_SERVER['SCRIPT_NAME']);
 if ($File3Name=="versioninfo.php"||$File3Name=="/versioninfo.php") {
@@ -27,8 +27,8 @@ function version_info($proname,$subver,$ver,$supver,$reltype,$svnver,$showsvn) {
 	return $return_var; }
 // Version number and date stuff. :P
 $VER1[0] = 0; $VER1[1] = 4; $VER1[2] = 7; $VERFull[1] = $VER1[0].".".$VER1[1].".".$VER1[2];
-$VER2[0] = "Alpha"; $VER2[1] = "Al"; $VER2[2] = "SVN"; $SubVerN = 745;
-$SVNDay[0] = 08; $SVNDay[1] = 08; $SVNDay[2] = 2011; $SVNDay[3] = $SVNDay[0]."/".$SVNDay[1]."/".$SVNDay[2];
+$VER2[0] = "Alpha"; $VER2[1] = "Al"; $VER2[2] = "SVN"; $SubVerN = 746;
+$SVNDay[0] = 08; $SVNDay[1] = 09; $SVNDay[2] = 2011; $SVNDay[3] = $SVNDay[0]."/".$SVNDay[1]."/".$SVNDay[2];
 $AltName = "RDB"; $AltName2 = "ReneeDB"; $RName = "iDB"; $SFName = "IntDB";
 $RFullName = "Internet Discussion Boards"; $AltFullName = "Renee Discussion Boards"; $AltGM2k = "ReneeDB Team";
 $VerCheckName = "iDB"; $AltVerCheckName = "RDB"; $AltiDBHome = "http://duckduckgo.com/?q=ReneeDB";
