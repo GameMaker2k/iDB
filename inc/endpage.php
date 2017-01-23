456110d6110293e278ef02c3a21fb83914bbac77
Kazuki Przyborowski <kazuki@localhost.localdomain>
2017-01-23 16:31:50 -0600

    Small bug fix

--- inc/endpage.php
+++ inc/endpage.php
@@ -11,7 +11,7 @@
     Copyright 2004-2015 iDB Support - http://idb.berlios.de/
     Copyright 2004-2015 Game Maker 2k - http://gamemaker2k.org/
 
-    $FileInfo: endpage.php - Last Update: 05/16/2016 SVN 806 - Author: cooldude2k $
+    $FileInfo: endpage.php - Last Update: 01/23/2017 SVN 807 - Author: cooldude2k $
 */
 $File3Name = basename($_SERVER['SCRIPT_NAME']);
 if ($File3Name=="endpage.php"||$File3Name=="/endpage.php") {
@@ -24,6 +24,11 @@ $MyTimeNow = $usercurtime->format($_SESSION['iDBTimeFormat']);
 $MyFullTimeNow = $usercurtime->format($_SESSION['iDBDateFormat'].", ".$_SESSION['iDBTimeFormat']);
 if(!isset($TimeSign)) { $TimeSign = ""; }
 $endpagevar=$endpagevar."<br />The time now is <span class=\"ctimenow\" title=\"".$MyFullTimeNow."\">".$MyTimeNow."</span> ".$ThemeSet['LineDivider']." All times are UTC ".$TimeSign." ".$MyDST; }
+if(function_exists("bcsub")==False) {
+function bcsub($left_operand, $right_operand, $scale = 0) {
+$lof = floatval($left_operand);
+$rof = floatval($right_operand);
+return sprintf("%0.".$scale."f", $lof - $rof); } }
 function execution_time($starttime) {
 list($uetime, $etime) = explode(" ", microtime());
 $endtime = $uetime + $etime;
