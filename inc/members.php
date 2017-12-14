66a46c93b48d6bf7cf34247f7aed77af71772d33
Joshua Thomas Przyborowski <joshua.przyborowski@gmail.com>
2017-12-13 19:59:17 -0600

    Update members.php

--- inc/members.php
+++ inc/members.php
@@ -1609,7 +1609,7 @@ require($SettDir['inc']."captcha.php"); }
 	<br />Your password is too big.<br />
 	</span>&nbsp;</td>
 </tr>
-<?php if (!filter_var($_POST['Email'], FILTER_VALIDATE_EMAIL)) { $Error="Yes";  ?>
+<?php } if (!filter_var($_POST['Email'], FILTER_VALIDATE_EMAIL)) { $Error="Yes";  ?>
 <tr>
 	<td><span class="TableMessage">
 	<br />Your email is not a valid email address.<br />
