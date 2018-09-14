83f01e3cdd685d8983aff17af7a4e8551eee4154
Kazuki Suzuki Przyborowski <kazuki.przyborowski@gmail.com>
2018-09-14 00:51:16 -0500

    Add files via upload

--- inc/admin/main.php
+++ inc/admin/main.php
@@ -11,7 +11,7 @@
     Copyright 2004-2017 iDB Support - http://idb.berlios.de/
     Copyright 2004-2017 Game Maker 2k - http://gamemaker2k.org/
 
-    $FileInfo: main.php - Last Update: 09/12/2018 SVN 869 - Author: cooldude2k $
+    $FileInfo: main.php - Last Update: 09/14/2018 SVN 878 - Author: cooldude2k $
 */
 $File3Name = basename($_SERVER['SCRIPT_NAME']);
 if ($File3Name=="main.php"||$File3Name=="/main.php") {
@@ -681,13 +681,13 @@ if(in_array("md2",hash_algos())) { ?>
 <option<?php if($Settings['use_hashtype']=="sha384") { echo " selected=\"selected\""; } ?> value="sha384">SHA384</option>
 <?php } if(in_array("sha512",hash_algos())) { ?>
 <option<?php if($Settings['use_hashtype']=="sha512") { echo " selected=\"selected\""; } ?> value="sha512">SHA512</option>
-<?php } if(in_array("sha3-sha224",hash_algos())) { ?>
+<?php } if(in_array("sha3-224",hash_algos())) { ?>
 <option<?php if($Settings['use_hashtype']=="sha3224") { echo " selected=\"selected\""; } ?> value="sha3224">SHA3-224</option>
-<?php } if(in_array("sha3-sha256",hash_algos())) { ?>
+<?php } if(in_array("sha3-256",hash_algos())) { ?>
 <option<?php if($Settings['use_hashtype']=="sha3256") { echo " selected=\"selected\""; } ?> value="sha3256">SHA3-256</option>
-<?php } if(in_array("sha3-sha384",hash_algos())) { ?>
+<?php } if(in_array("sha3-384",hash_algos())) { ?>
 <option<?php if($Settings['use_hashtype']=="sha3384") { echo " selected=\"selected\""; } ?> value="sha3384">SHA3-384</option>
-<?php } if(in_array("sha3-sha512",hash_algos())) { ?>
+<?php } if(in_array("sha3-512",hash_algos())) { ?>
 <option<?php if($Settings['use_hashtype']=="sha3512") { echo " selected=\"selected\""; } ?> value="sha3512">SHA3-512</option>
 <?php } if(in_array("ripemd128",hash_algos())) { ?>
 <option<?php if($Settings['use_hashtype']=="ripemd128") { echo " selected=\"selected\""; } ?> value="ripemd128">RIPEMD128</option>
