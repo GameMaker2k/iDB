deb6b70b017c99e326cf90b118d44f878faa9b1c
Kazuki Suzuki Przyborowski <kazuki.przyborowski@gmail.com>
2024-08-26 20:58:36 -0500

    Update mkconfig.php

--- setup/mkconfig.php
+++ setup/mkconfig.php
@@ -281,7 +281,6 @@ if($Settings['charset']=="UTF-8") {
 	$SQLCharset = "utf8mb4"; } 
 $Settings['sql_collate'] = $SQLCollate;
 $Settings['sql_charset'] = $SQLCharset; }
-var_dump($SQLCharset,$SQLStat);
 sql_set_charset($SQLCharset,$SQLStat);
 if($SQLStat===false) { $Error="Yes";
 echo "<br />".sql_errorno($SQLStat)."\n"; }
