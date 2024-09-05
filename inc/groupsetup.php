cc73ee64bb261b72e5cb03e9ea1648ba119705b4
Kazuki Suzuki Przyborowski <kazuki.przyborowski@gmail.com>
2024-09-05 01:39:05 -0500

    Add files via upload

--- inc/groupsetup.php
+++ inc/groupsetup.php
@@ -174,6 +174,9 @@ if(!is_numeric($LevelInfo['DemotePosts'])) {
 $LevelInfo['DemoteKarma']=sql_result($levresult,0,"DemoteKarma");
 if(!is_numeric($LevelInfo['DemoteKarma'])) { 
 	$LevelInfo['DemoteKarma'] = 0; $LevelInfo['DemoteTo'] = 0; } }
+elseif($levnum<=0)&&$ChkUsrLevelID!=0) {
+	$queryupgrade = sql_pre_query("UPDATE \"".$Settings['sqltable']."members\" SET \"LevelID\"=0 WHERE \"id\"=%i", array($_SESSION['UserID']));
+	sql_query($queryupgrade,$SQLStat); }
 if($_SESSION['UserID']==0||$_SESSION['UserID']==null) {
  $rannum = 0; }
 if($_SESSION['UserID']!=0&&$_SESSION['UserID']!=null) {
@@ -203,6 +206,9 @@ if(!is_numeric($RankInfo['DemotePosts'])) {
 $RankInfo['DemoteKarma']=sql_result($ranresult,0,"DemoteKarma");
 if(!is_numeric($RankInfo['DemoteKarma'])) { 
 	$RankInfo['DemoteKarma'] = 0; } }
+elseif($rannum<=0)&&$ChkUsrRankID!=0) {
+	$queryupgrade = sql_pre_query("UPDATE \"".$Settings['sqltable']."members\" SET \"RankID\"=0 WHERE \"id\"=%i", array($_SESSION['UserID']));
+	sql_query($queryupgrade,$SQLStat); }
 // Member Group Setup
 if(!isset($_SESSION['UserGroup'])) { $_SESSION['UserGroup'] = null; }
 if($_SESSION['UserGroup']==null) { 
